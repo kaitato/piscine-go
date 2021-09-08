61d3a0424832fa57697d99ba2dfbd355ceebc616
karyun.cheung <karyun.cheung@gmail.com>
2021-09-08 14:14:27 +0100

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -8,16 +8,17 @@ func PrintComb2() {
 			for c := a; c <= '9'; c++ {
 				for d := b + 1; d <= '9'; d++ {
 					if d > b && a <= c {
-						z01.PrintRune('a')
-						z01.PrintRune('b')
+						z01.PrintRune(a)
+						z01.PrintRune(b)
 						z01.PrintRune(' ')
-						z01.PrintRune('c')
-						z01.PrintRune('d')
-						} if a == '9' & b == '8' {
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						if a == '9' && b == '8' {
 							z01.PrintRune('\n')
-					} else {
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+						} else {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 					}
 				}
 			}
