4a4289ed71f6cbd16fedbc79bdaaf6ea5ba021a4
karyun.cheung <karyun.cheung@gmail.com>
2021-09-08 19:40:33 +0100

    printstr

--- printstr.go
+++ printstr.go
@@ -2,8 +2,6 @@ package piscine
 
 import import "github.com/01-edu/z01"
 
-func PrintStr(s string) { 
-	for i := 0; i <= 12; i++ {
-		z01.PrintRune(string(Hello World!)[i])
-	}
+func PrintStr(s string) {
+	z01.PrintRune(%s, string)
 }
