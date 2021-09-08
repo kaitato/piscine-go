1120218838cc978432003229c5eb7ed95a48d323
karyun.cheung <karyun.cheung@gmail.com>
2021-09-08 11:31:29 +0100

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,15 +3,15 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := 0; a < 7; a++ {
-		for b := a + 1; b <= 9; b++ {
-			for c := b + 1; c <= 9; c++ {
+	for a := '0'; a < '7'; a++ {
+		for b := a + '1'; b <= '9'; b++ {
+			for c := b + '1'; c <= '9'; c++ {
 				if a < b {
 					if b < c {
 						z01.PrintRune('a')
 						z01.PrintRune('b')
 						z01.PrintRune('c')
-						if a < 7 {
+						if a < '7' {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						} else {
