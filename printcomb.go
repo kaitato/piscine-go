75c533843747bb95b9a4d25ab49c0d43b2f8dac2
karyun.cheung <karyun.cheung@gmail.com>
2021-09-08 12:10:59 +0100

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for a := '0'; a <= '7'; a++ {
-		for b := a + '1'; b <= '9'; b++ {
+		for b := a + '1'; b <= '8'; b++ {
 			for c := b + '1'; c <= '9'; c++ {
 				if a < b {
 					if b < c {
