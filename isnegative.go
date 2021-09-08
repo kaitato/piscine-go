7453c994a3af83ce326ffe203d5d43fb58ec0e26
karyun.cheung <karyun.cheung@gmail.com>
2021-09-08 09:55:12 +0100

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -3,10 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	if int < 0 {
-		z01.PrintRune("T")
+	if IsNegative < 0 {
+		z01.PrintRune('T')
 	} else {
-		z01.PrintRune("F")
+		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
 }
