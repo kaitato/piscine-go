04d8f7574a83fd5db8c93c7851be23d8a02f5f55
karyun.cheung <karyun.cheung@gmail.com>
2021-09-09 14:57:19 +0100

    strrev

--- strrev.go
+++ strrev.go
@@ -1,9 +1,9 @@
 package piscine
 
 func StrRev(s string) string {
-	chars := []rune(s)]
-	for i, j := 0, len*chars)-1; i < j; i, j = i + 1, j - 1 {
-		chars[i], chars[j] + chars[j], chars[i]
+	a := []rune(s)]
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
 	}
-	return string(chars)
+	return string(a)
 }
