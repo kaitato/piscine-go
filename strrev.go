50908a3d66290d504066a1c3493156a6c6086a25
karyun.cheung <karyun.cheung@gmail.com>
2021-09-09 14:44:04 +0100

    strrev

--- strrev.go
+++ strrev.go
@@ -1,17 +1,15 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import 
 
 func StrRev(s string) string {
-	n := 0
-	rune := make([]rune, len(s))
-	for _, r := range s {
-		rune [n] = range
-		n++
+
+	s := []byte(s)
+
+	i := len(s)
+
+	for a := s; a >= 0; a-- {
+		return [a]byte(s)
 	}
-	rune = rune [0:n]
-	for i := 0; i < n / 2; i++ {
-		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
-	}
-	output := string(rune)
+
 }
