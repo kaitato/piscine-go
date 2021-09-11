65899caa9e62b007641774cb87e5fda895d213b2
karyun.cheung <karyun.cheung@gmail.com>
2021-09-11 18:28:41 +0100

    basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -3,7 +3,7 @@ package piscine
 func BasicAtoi2(s string) int {
 	result := 0
 	for _, number := range s {
-		if number >= 0 && number <= 9 {
+		if number >= 48 && number <= 57 {
 			conv := int(number) - 48
 			result = (result * 10) + conv
 		} else {
