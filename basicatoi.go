08329692f9fe470085cf943284e1c389808feca1
karyun.cheung <karyun.cheung@gmail.com>
2021-09-11 18:14:29 +0100

    atoi

--- basicatoi.go
+++ basicatoi.go
@@ -4,7 +4,7 @@ func BasicAtoi(s string) int {
 	result := 0
 	for _, number := range s {
 		conv := int(number) - 48
-		result = result*10 + conv
+		result = (result * 10) + conv
 	}
 	return result
 }
