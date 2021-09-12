5ac45f1fcced86123aea67e05a8618bea4fb7364
karyun.cheung <karyun.cheung@gmail.com>
2021-09-12 16:00:39 +0100

    iterativepower

--- iterativepower.go
+++ iterativepower.go
@@ -7,14 +7,12 @@ func main() {
 }
 
 func IterativePower(nb int, power int) int {
-	if nb < 0 || nb > 25 {
+	a := nb
+	if nb < 0 {
 		return 0
-	} else if nb == 0 {
-		return 1
 	}
-	result := 1
-	for i := 1; i <= power; i++ {
-		result = result * i
+	for i := 1; i < power; i++ {
+		a *= nb
 	}
-	return result
+	return a
 }
