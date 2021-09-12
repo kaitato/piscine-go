739b2d895ea21e424870bd0b9909509cc5287f0c
karyun.cheung <karyun.cheung@gmail.com>
2021-09-12 15:27:22 +0100

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,14 +1,14 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 13 {
+	if nb < 0 || nb > 12 {
 		return 0
 	} else if nb == 0 {
 		return 1
 	}
-	a := nb - 1
-	for i := a; i >= 1; i-- {
-		nb *= i
+	result := 1
+	for i := 1; i <= nb; i++ {
+		result = result * i
 	}
-	return nb
+	return result
 }
