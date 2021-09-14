db63bae05ffaac1c513fd70168f1bd9b819945bd
karyun.cheung <karyun.cheung@gmail.com>
2021-09-14 23:36:14 +0100

    capitalize

---
+++ capitalize.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Capitalize("Hello! How are you? How+are+things+4you?"))
+}
+
+func Capitalize(s string) string {
+	runeArray := []rune(s)
+	for i, char := range runeArray {
+		if isNumberOrAlph(char) {
+			if i == 0 || isNumberOrAlph(runeArray[i-1]) == false {
+				if runeArray[i] >= 'a' && runeArray[i] <= 'z' {
+					runeArray[i] = char - 32
+				}
+			} else {
+				if runeArray[i] >= 'A' && runeArray[i] <= 'Z' {
+					runeArray[i] = char + 32
+				}
+			}
+		}
+	}
+	return string(runeArray)
+}
+
+func isNumberOrAlph(r rune) bool {
+	if r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' {
+		return true
+	}
+	return false
+}
