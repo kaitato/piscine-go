ac32bf5984a143fef807596b8864fc91ea03ea8e
karyun.cheung <karyun.cheung@gmail.com>
2021-09-14 15:54:37 +0100

    yes

--- islower.go
+++ islower.go
@@ -1,14 +1,14 @@
 package piscine
 
 func IsLower(s string) bool {
-
+	length := len(s)
 	count := 0
 	for _, caps := range s {
 		if caps >= 'a' && caps <= 'z' {
 			count++
 		}
 	}
-	if count == len(s) {
+	if count == length {
 		return true
 	}
 	return false
