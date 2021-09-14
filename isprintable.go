356a96f37f9f50d10384455b2016ed415b9b1fd9
karyun.cheung <karyun.cheung@gmail.com>
2021-09-14 15:03:03 +0100

    is printable

---
+++ isprintable.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsPrintable(s string) bool {
+	length := len(s)
+	count := 0
+	for _, caps := range s {
+		if caps > 32 {
+			count++
+		}
+	}
+	if count == length {
+		return true
+	}
+	return false
+}
