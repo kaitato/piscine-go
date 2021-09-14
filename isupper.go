4b79f1b17b5e48de81a277abe8c27481c09c107a
karyun.cheung <karyun.cheung@gmail.com>
2021-09-14 15:26:14 +0100

    is upper

--- isupper.go
+++ isupper.go
@@ -1,14 +1,14 @@
 package piscine
 
 func IsUpper(s string) bool {
-	aRune := len(s)
+
 	count := 0
-	for _, caps := range s {
-		if caps >= 'A' && caps <= 'Z' {
+	for _, char := range s {
+		if char >= 'A' && char <= 'Z' {
 			count++
 		}
 	}
-	if count == aRune {
+	if count == len(s) {
 		return true
 	}
 	return false
