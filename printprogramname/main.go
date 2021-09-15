dacc9ef438ab273178c4251745cf829f1fe780d8
karyun.cheung <karyun.cheung@gmail.com>
2021-09-15 18:09:32 +0100

    print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,6 +7,16 @@ import (
 )
 
 func main() {
-	arg := os.Args[0]
-	z01.PrintRune(arg)
+	arg := []rune(os.Args[0])
+	a := 1
+	for i := len(arg) - 1; i >= 0; i-- {
+		if arg[i] == '/' {
+			a += i
+			break
+		}
+	}
+	for j := a; j < len(arg); j++ {
+		z01.PrintRune(arg[j])
+	}
+	z01.PrintRune('\n')
 }
