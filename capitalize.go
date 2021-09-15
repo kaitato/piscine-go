28c39fb3a3a4d4445a42e00cdf540f52fefe7d51
karyun.cheung <karyun.cheung@gmail.com>
2021-09-15 11:56:56 +0100

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -2,6 +2,12 @@ package piscine
 
 func Capitalize(s string) string {
 	aRune := []rune(s)
+	for i := range aRune {
+		if aRune[i] >= 'A' && aRune[i] <= 'Z' {
+			aRune[i] = aRune[i] + 32
+		}
+	}
+
 	for i, char := range aRune {
 		if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' || char >= '0' && char <= '9' {
 			if i == 0 || aRune[i-1] >= 'a' && aRune[i-1] <= 'z' || aRune[i-1] >= 'A' && aRune[i-1] <= 'Z' || aRune[i-1] >= '0' && aRune[i-1] <= '9' {
