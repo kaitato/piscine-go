549c06f42447f2cabf284f31f0269b2caed93264
karyun.cheung <karyun.cheung@gmail.com>
2021-09-19 14:05:51 +0100

    append range

--- appendrange.go
+++ appendrange.go
@@ -2,7 +2,7 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	var result []int
-	for i := min; i <= max-1; i++ {
+	for i := min; i < max; i++ {
 		result = append(result, i+1)
 	}
 	return result
