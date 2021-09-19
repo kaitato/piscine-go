3c080bac23efce8617f3ec447ae96721cd00bad9
karyun.cheung <karyun.cheung@gmail.com>
2021-09-19 14:38:31 +0100

    append range

--- appendrange.go
+++ appendrange.go
@@ -4,9 +4,7 @@ func AppendRange(min, max int) []int {
 	var result []int
 	for i := min; i < max-1; i++ {
 		if min > 0 {
-			result = append(result, i+1)
-		} else {
-			return result
+			result = append(result, i)
 		}
 	}
 	return result
