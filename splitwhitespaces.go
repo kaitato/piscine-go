667a03bf1765db9a1b46376fc5ea4cb646b0a697
karyun.cheung <karyun.cheung@gmail.com>
2021-09-19 15:15:31 +0100

    split white spaces

---
+++ splitwhitespaces.go
@@ -0,0 +1,38 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var aString []string
+	count := 1
+	strlen := 0
+	result := ""
+	for c := range s {
+		if isWhiteSpace(s[c]) {
+			count++
+		}
+		strlen++
+	}
+	aString = make([]string, count)
+	i := 0
+	for j, c := range s {
+		if j+1 == strlen {
+			aString[i] = result + string(s[j])
+		}
+		if isWhiteSpace(s[j]) {
+			if i <= count {
+				aString[i] = result
+				i++
+				result = ""
+			}
+		} else {
+			result += string(c)
+		}
+	}
+	return aString
+}
+
+func isWhiteSpace(r byte) bool {
+	if r == ' ' || r == '\n' || r == '\t' {
+		return true
+	}
+	return false
+}
