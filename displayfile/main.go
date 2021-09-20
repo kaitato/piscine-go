01e74d62a0fa6f4b953854415f6a372c621b8482
karyun.cheung <karyun.cheung@gmail.com>
2021-09-20 16:27:19 +0100

    display file

--- displayfile/main.go
+++ displayfile/main.go
@@ -11,18 +11,18 @@ func main() {
 
 	length := 0
 	for i := range args {
-		length = i
+		length = i + 1
 	}
 
-	if length >= 1 {
+	if length > 1 {
 		fmt.Println("Too many arguments")
 	} else if length == 0 {
 		fmt.Println("File name missing")
 	} else if args[0] == "quest8.txt" {
 
-		contents, e := ioutil.ReadFile(args[0])
-		if e != nil {
-			fmt.Println(e.Error())
+		contents, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			fmt.Println(err.Error())
 			return
 		}
 		fmt.Println(string(contents))
