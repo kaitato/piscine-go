a1924ae9e13ca02e5264dcf316648185ecb9c302
karyun.cheung <karyun.cheung@gmail.com>
2021-09-22 15:13:54 +0100

    com check

--- comcheck/main.go
+++ comcheck/main.go
@@ -6,23 +6,12 @@ import (
 )
 
 func main() {
-	var aRune []rune
-	var result string
-	word := []string{"01", "galaxy", "galaxy 01"}
-
-	for i := 1; i < len(os.Args); i++ {
-		aRune = []rune(os.Args[i])
-	}
-
-	for j := 0; j < len(aRune); j++ {
-		if aRune[j] != ' ' {
-			result += string(aRune[j])
-		}
-	}
-
-	for _, s := range word {
-		if result == s {
-			fmt.Println("Alert!!!")
+	if len(os.Args) > 1 {
+		for _, i := range os.Args {
+			if i == "01" || i == "galaxy" || i == "galaxy 01" {
+				fmt.Println("Alert!!!")
+				break
+			}
 		}
 	}
 }
