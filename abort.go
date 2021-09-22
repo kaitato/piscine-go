94e8ca004f8293b6c6220e5cc37809e754ca429e
karyun.cheung <karyun.cheung@gmail.com>
2021-09-22 11:37:19 +0100

    abort

--- abort.go
+++ abort.go
@@ -7,5 +7,21 @@ func Abort(a, b, c, d, e int) int {
 			arg[i], arg[i-1] = arg[i-1], arg[i]
 		}
 	}
+	for i := 1; i < len(arg); i++ {
+		if arg[i] < arg[i-1] {
+			arg[i], arg[i-1] = arg[i-1], arg[i]
+		}
+	}
+	for i := 1; i < len(arg); i++ {
+		if arg[i] < arg[i-1] {
+			arg[i], arg[i-1] = arg[i-1], arg[i]
+		}
+	}
+	for i := 1; i < len(arg); i++ {
+		if arg[i] < arg[i-1] {
+			arg[i], arg[i-1] = arg[i-1], arg[i]
+		}
+	}
+
 	return arg[2]
 }
