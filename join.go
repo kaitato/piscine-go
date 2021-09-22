e2cf1e345346f4dcfff48e0485e57b0bfa83b45e
karyun.cheung <karyun.cheung@gmail.com>
2021-09-22 18:53:31 +0100

    join

--- join.go
+++ join.go
@@ -1,13 +1,11 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func Join(strs []string, sep string) string {
 	result := ""
 	for index, letters := range strs {
 		result += letters
 		if strs[index] == strs[len(strs)-1] {
-			z01.PrintRune('\n')
+			break
 		} else {
 			result += sep
 		}
