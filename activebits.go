b1a9269aaa367e2c99abeba4642be666a70b2954
karyun.cheung <karyun.cheung@gmail.com>
2021-09-22 17:43:44 +0100

    active bits

--- activebits.go
+++ activebits.go
@@ -2,11 +2,14 @@ package piscine
 
 func ActiveBits(n int) int {
 	count := 0
+	if n < 1 || n > 64 {
+		return 0
+	}
 	for n > 0 {
 		if n%2 == 1 {
-			n = n / 2
 			count++
 		}
+		n = n / 2
 	}
 	return count
 }
